2270285d2bb195311f830a9025f5c74ae99cf04b
Pedro Alexander Garcia A <pedroalexandergarciaa@Pedros-iMac.local>
2023-04-13 15:11:27 -0400

    spelling issues

--- myscript.js
+++ myscript.js
@@ -1,5 +1,18 @@
+// A variable is a container for storing data
+// A variable behaves as if it was the value that it contains
+
+// Two steps:
+// 1. Declaration (var, let, const)
+// 2. Assigment (= assignment operator)
+
+let firstName = "Garcia!"; //string
+let age = 24; //number
+
+console.log("Hello", firstName)//In "" for adding text after a coma
+console.log(age);// Pop out in the console panel
+
 function sayHello() {
-    alert("Hello, world!");
+    alert("Hello, My First Personal Website!");// Pop out a message alert
   }
   sayHello()
   const form = document.querySelector("#form")
